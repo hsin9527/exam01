0045fb473fae6391c39f4c88d0cbe0c478649f64
ee2405 <dean03318Wgmail.com>
2020-04-01 17:35:51 +0800

    3

---
+++ ex1.cpp
@@ -0,0 +1,31 @@
+#include "mbed.h"
+
+
+PwmOut PWM1(D6);
+
+//int sample = 128;
+int i;
+
+//float ADCdata[128];
+
+int main(){
+    PWM1.period(1/1000);
+    for( i=0; i<1; i+=0.1 ){
+        PWM1 = i;
+        wait(0.01);
+    }
+    for(i=1; i>0; i-=0.1){
+        PWM1 = i;
+        wait(0.1);
+    }
+  for (i = 0; i < sample; i++){
+    Aout = Ain;
+    ADCdata[i] = Ain;
+    wait(1./sample);
+    
+  }
+  for (i = 0; i < sample; i++){
+    pc.printf("%1.3f\r\n", ADCdata[i]);
+    wait(0.1);
+  }
+}
